16521f047ebc2b6605c3765e0474253cf8b5ec38
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-10-11 11:59:39 +0200

    Added Logic to Navigate to Detail Page

--- FavorieteFilm/MovieList.js
+++ FavorieteFilm/MovieList.js
@@ -1,33 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  StyleSheet,
-  Dimensions,
-  useWindowDimensions,
-} from 'react-native';
+import React from 'react';
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const MovieList = ({ data }) => {
-  const windowWidth = useWindowDimensions().width;
-  const numColumns = Math.floor(windowWidth / 350); // Adjust 150 as needed
+  const navigation = useNavigation();
 
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id.toString()}
-      numColumns={numColumns}
+      // numColumns={numColumns}
       renderItem={({ item }) => (
-        <View style={styles.movieItem}>
+        <TouchableOpacity
+          style={styles.movieItem}
+          onPress={() => {
+            navigation.navigate('MovieDetails', { movie: item });
+          }}
+        >
           <Image
             source={{
               uri: `https://image.tmdb.org/t/p/original${item.poster_path}`,
             }}
             style={styles.poster}
           />
-          {/* <Text style={styles.title}>{item.title}</Text> */}
-        </View>
+        </TouchableOpacity>
       )}
     />
   );
@@ -42,14 +38,9 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   poster: {
-    width: '100%', // Occupies the full width of the column
-    aspectRatio: 2 / 3, // Adjust the aspect ratio as needed
+    width: '100%', 
+    aspectRatio: 2 / 3, 
     resizeMode: 'contain',
-    // shadowColor: 'rgba(20, 20, 20, 3)',
-    // shadowOffset: {width: 2.5, height: 3},
-    // shadowOpacity: 1,
-    // shadowRadius: 0,
-    // marginBottom: 10,
   },
   title: {
     fontSize: 16,
