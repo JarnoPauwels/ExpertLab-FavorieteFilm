41e6e4f1b19bf9d76ff9f6d2403321f1d5c6b24b
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-10-16 15:18:31 +0200

    Added Get And Styling
    Added GET to get the watchlist and basic styling

--- FavorieteFilm/WatchList.js
+++ FavorieteFilm/WatchList.js
@@ -1,22 +1,77 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
+import axios from 'axios';
 
-const Watchlist = ({ watchlist }) => {
+const Watchlist = () => {
+  const [watchlist, setWatchlist] = useState([]);
+
+  const fetchWatchlistData = async () => {
+    try {
+      const apiUrl = 'http://localhost:3000/watchlist';
+
+      const response = await axios.get(apiUrl);
+
+      const watchlistData = response.data;
+
+      setWatchlist(watchlistData);
+    } catch (error) {
+      console.error('Error fetching watchlist data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWatchlistData();
+  }, []);
+
   return (
-    <View>
-      <Text>Watchlist</Text>
+    <View style={styles.container}>
+      {/* <Text style={styles.title}>Watchlist</Text> */}
       <FlatList
         data={watchlist}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item) => item.movie_id.toString()}
         renderItem={({ item }) => (
           <View>
-            {/* Render movie details here */}
-            <Text>{item.title}</Text>
+            <Text style={styles.movieText}>{item.title}</Text>
+            <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/original${item.poster}`,
+            }}
+            style={styles.poster}
+            />
+            <Text style={styles.movieDescription}>{item.description}</Text>
           </View>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  movieText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  movieDescription: {
+    color: 'white',
+    fontSize: 16,
+  },
+  poster: {
+    width: '100%', 
+    aspectRatio: 2 / 3,
+    resizeMode: 'contain',
+  },
+});
 
 export default Watchlist;
